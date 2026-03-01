383cc2ebf8faa7873acbcbbd6b3ea35e2ae70df3
dev-j0e1 <corkj9213@gmail.com>
2026-03-01 20:24:09 +0800

    basically functional now. Have the ability to type the letters and it shows the letter in the tile as well as showing the colour representing how correct it is.

--- main.js
+++ main.js
@@ -9,7 +9,6 @@ function createTile(letter) {
     let letterTile = document.createElement("div")  
     letterTile.innerText = letter ?? ""
     letterTile.classList.add("letter-tile")
-    letterTile.contentEditable = true
     return letterTile
 }
 
@@ -30,13 +29,68 @@ function renderWordToContainer(word, numberOfGuesses) {
 
 }
 
+class Guess {
+    constructor(name, letters, complete) {
+        this.name = name 
+        this.letters = letters 
+        this.complete = complete 
+    }
+}
+
+function findCurrentRowIndex(array) {
+    for (let i = 0; i<array.length; i++) {
+        if (!array[i].complete){
+            return i         
+        }
+    }
+}
+
+function fillCurrentTile(rowIndex, tileIndex, value, className) {
+    let currentRow = document.querySelectorAll(".word-row")[rowIndex]
+    let currentTile = currentRow.querySelectorAll(".letter-tile")[tileIndex]
+    currentTile.innerText = value
+    currentTile.classList.add(className)
+}
+
+function isLetterCorrect(index, letter, correctWord) {
+    if (correctWord[index].toLowerCase() === letter.toLowerCase()) {
+        return "correct"
+    } else {
+        if (correctWord.toLowerCase().includes(letter.toLowerCase())) {
+            return "almost"
+        } else {
+            return "nope"
+        }
+    }
+}
+
 window.addEventListener("load",e=>{
     let theWord = theList[getRandomNumber(0, theList.length-1)]
     let numberOfGuesses = 5
     console.log(theWord)
+    let alphabet = Array.from({length: 26},(_,i)=>String.fromCharCode(97+i))
+    window.data = [] 
+    for (let i = 0; i<numberOfGuesses; i++) {
+        window.data.push(new Guess(`guess ${i+1}`, [], false))
+    }
+
     renderWordToContainer(theWord, numberOfGuesses)
 
+    document.addEventListener("keydown", e=>{
+        if (alphabet.includes(e.key.toLowerCase())) {
+            let currentRowIndex = findCurrentRowIndex(window.data)
+            let currentGuess = window.data[currentRowIndex] 
+            currentGuess.letters.push(e.key.toUpperCase())
+            fillCurrentTile(
+                currentRowIndex,
+                currentGuess.letters.length-1,
+                e.key.toUpperCase(),
+                isLetterCorrect(currentGuess.letters.length-1, e.key, theWord)
+            )
+            if (currentGuess.letters.length === theWord.length) {
+                currentGuess.complete = true
+            }
+        }
 
-
-
+    })
 })
