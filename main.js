ef9f03ade46388e577321a548e2cad01d221cd1c
dev-j0e1 <corkj9213@gmail.com>
2026-03-01 13:30:50 +0800

    added tiling system and content editable :)

--- main.js
+++ main.js
@@ -5,23 +5,36 @@ function getRandomNumber(min, max) {
   return parseInt(Math.random() * (max - min) + min);
 }
 
-function addTile(letter) {
-    let wordlContainer = document.querySelector("#grid-container")
+function createTile(letter) {
     let letterTile = document.createElement("div")  
-    letterTile.innerText = letter 
+    letterTile.innerText = letter ?? ""
     letterTile.classList.add("letter-tile")
-    wordlContainer.append(letterTile)
+    letterTile.contentEditable = true
+    return letterTile
 }
 
-function renderWordToContainer(word) {
-    let letters = [...word]
-    letters.forEach(e=>addTile(e))
+function createRow(numberOfLetters) {
+    let newRow = document.createElement("div")
+    newRow.classList.add("word-row")
+    for (let i = 0; i<numberOfLetters; i++) {
+        newRow.append(createTile())
+    }
+    return newRow
+}
+
+function renderWordToContainer(word, numberOfGuesses) {
+    let wordlContainer = document.querySelector("#grid-container")
+    for (let i = 0; i<numberOfGuesses; i++) {
+        wordlContainer.append(createRow(word.length))
+    }
+
 }
 
 window.addEventListener("load",e=>{
     let theWord = theList[getRandomNumber(0, theList.length-1)]
+    let numberOfGuesses = 5
     console.log(theWord)
-    renderWordToContainer(theWord)
+    renderWordToContainer(theWord, numberOfGuesses)
 
 
 
